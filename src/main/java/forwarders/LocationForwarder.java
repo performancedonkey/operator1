4d163d41139d119e09da5357ad118349f883943d
Performance Donkey <performancedonkey@gmail.com>
2022-09-13 18:57:41 -0300

    First try

---
+++ src/main/java/forwarders/LocationForwarder.java
@@ -0,0 +1,44 @@
+package forwarders;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendAudio;
+import org.telegram.telegrambots.meta.api.methods.send.SendLocation;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Mati
+ * Date: 2022-09-12
+ */
+public class LocationForwarder implements Forwarder {
+    private final SendLocation sender = new SendLocation();
+
+    @Override public boolean hasMedia(Update update) {
+        return update.getMessage().hasLocation();
+    }
+
+    @Override public String getId(Update update) {
+        return update.getMessage().getLocation().toString();
+    }
+
+    @Override public void prepare(long targetId, Update update, InputFile inputFile) {
+        sender.setChatId(targetId);
+        sender.setLatitude(update.getMessage().getLocation().getLatitude());
+        sender.setLongitude(update.getMessage().getLocation().getLongitude());
+//        sender.setLongitude(update.getMessage().getLocation().getLongitude());
+    }
+
+    @Override public void forward(TelegramLongPollingBot bot) {
+        try {
+            bot.execute(sender);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override public String getText(Update update) {
+        return "Location";
+    }
+}
